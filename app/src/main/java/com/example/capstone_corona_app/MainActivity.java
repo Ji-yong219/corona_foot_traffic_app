c472749b8ef56e91a19be0b45a36f899b00b34af
Ji-yong219 <76960714+Ji-yong219@users.noreply.github.com>
2021-08-29 19:50:25 +0900

    add bottom icon

--- app/src/main/java/com/example/capstone_corona_app/MainActivity.java
+++ app/src/main/java/com/example/capstone_corona_app/MainActivity.java
@@ -128,12 +128,12 @@ public class MainActivity extends AppCompatActivity implements
         startActivity(intent);
 
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_location, R.id.navigation_data, R.id.navigation_setting)
-                .build();
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_location, R.id.navigation_data, R.id.navigation_setting)
+//                .build();
 //        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 //        NavigationUI.setupWithNavController(navView, navController);
